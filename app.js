f5f7ea629d882028b739a1b08547ae3fd8f1076a
alvinnzz <alvinng01@live.com>
2024-09-22 22:24:19 +0800

    fix: middleware and port number

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const cafeRoutes = require("./routes/cafe");
 const employeeRoutes = require("./routes/employee");
 const mongoose = require("mongoose");
 
 //Midlewares
+app.use(cors());
 app.use(express.json());
 
 //Routes
 app.use("/cafe", cafeRoutes);
 app.use("/employee", employeeRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 
 mongoose
   .connect(process.env.MONGO_URI)
